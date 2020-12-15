2276338559fbfbe6a3ae8472e3b19096afca6568
granausthyn <60592262+granausthyn@users.noreply.github.com>
2020-12-15 21:32:50 +0800

    update

---
+++ CpuScheduling.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class CpuScheduling {
+    static Scanner UserInput = new Scanner(System.in);
+    public static void main(String[] args) {
+
+    }
+
+    static void userOptions(){
+
+        System.out.println("\nChoose CPU Scheduling [Non-Preemptive]Algorithm: ");
+        System.out.println("[A] (FCFS)");
+        System.out.println("[B] (SJF)");
+        System.out.println("[C] Priority");
+        System.out.println("[D] Deadline");
+        System.out.println("[E] (MLQ)");
+        System.out.println("[G] Exit\n");
+        System.out.print("Choose Algorithm: ");
+    }
+}
