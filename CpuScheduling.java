bedfd0b870330651183ea82ffebf0cd2873c12c6
granausthyn <60592262+granausthyn@users.noreply.github.com>
2020-12-16 00:11:47 +0800

    update ule

--- CpuScheduling.java
+++ CpuScheduling.java
@@ -6,7 +6,7 @@ public class CpuScheduling {
     
     
     public static void main(String[] args) {
-
+        displayUserOptions();
     }
 
     public void chooseOption(){
--- FCFS.java
+++ FCFS.java
@@ -1,3 +1,5 @@
+// FCFS algo
+
 import java.util.Scanner;  
 
 public class FCFS{
