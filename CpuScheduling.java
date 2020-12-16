1b18c62a326ffedf0182157b9ac9d2f9cfd55a71
granausthyn <60592262+granausthyn@users.noreply.github.com>
2020-12-16 23:07:25 +0800

    commit

--- CpuScheduling.java
+++ CpuScheduling.java
@@ -25,23 +25,32 @@ public class CpuScheduling {
 
     //get user input
     static void chooseAlgorithm() {
-        System.out.print("Enter Choice: ");
-        inputAlgorithm = sc.next().toUpperCase();
-        //if user input is A-F, proceed with the program
-        if(inputAlgorithm.equalsIgnoreCase("A") || inputAlgorithm.equalsIgnoreCase("B") || inputAlgorithm.equalsIgnoreCase("C") ||
-        inputAlgorithm.equalsIgnoreCase("D") || inputAlgorithm.equalsIgnoreCase("E") || inputAlgorithm.equalsIgnoreCase("F")){
-            if(inputAlgorithm.equalsIgnoreCase("F")){
-                //if user chooses F, the program will terminate
-                System.out.println("Terminating Program.........");
-                System.exit(0);
-            }
-            //if answer is A-E, proceed with program
-        }
-        //if user input is not A-F, the method will reiterate
-        else{
-            chooseAlgorithm();
-        }
 
+        boolean isChoice = false;
+        //validate user input to enter correct option. system will loop if user input does not match the given choices.
+        
+        do{
+            System.out.print("Enter Choice: ");
+            inputAlgorithm = sc.next().toUpperCase();
+            //if user input is A-F, proceed with the program
+            if(inputAlgorithm.equalsIgnoreCase("A") || inputAlgorithm.equalsIgnoreCase("B") || inputAlgorithm.equalsIgnoreCase("C") ||
+            inputAlgorithm.equalsIgnoreCase("D") || inputAlgorithm.equalsIgnoreCase("E") || inputAlgorithm.equalsIgnoreCase("F")){
+                if(inputAlgorithm.equalsIgnoreCase("F")){
+                    //if user chooses F, the program will terminate
+                    System.out.println("Terminating Program.........");
+                    System.exit(0);
+                }
+                //if answer is A-E, proceed with program
+                isChoice = true;
+            }
+            //if user input is not A-F, the method will reiterate
+            else{
+                isChoice  = false;
+                
+            }
+    
+        }while(isChoice == false);
+        
         //check algorithm chosen by the user
         switch (inputAlgorithm) {
             
